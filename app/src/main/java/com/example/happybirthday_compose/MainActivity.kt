a88a1ba05fa09552bd734dfbc9e5184c034bac11
Aleksey Lichtman <alichtman@sram.com>
2023-04-24 13:10:57 -0500

    finished the view

--- app/src/main/java/com/example/happybirthday_compose/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday_compose/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.happybirthday_compose
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday_compose.ui.theme.HappyBirthday_ComposeTheme
 
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
             HappyBirthday_ComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    BirthdayGreetingWithText("Happy birthday, Yoba","Yeboba")
+                    BirthdayGreetingWithImage("Happy birthday, Yoba","Yeboba")
                 }
             }
         }
@@ -30,15 +34,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    //Step 3 create a box to overlap image and texts
+    Box {
+        Image(
+            painter = painterResource(R.drawable.androidparty),
+            contentScale = ContentScale.Crop,
+            contentDescription = null
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+
+@Composable
 fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = message,
             fontSize = 36.sp
         )
+        Spacer(modifier = Modifier.weight(1.0f))
         Text(
             text = from,
-            fontSize =  24.sp
+            fontSize =  24.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .align(Alignment.End)
         )
     }
 }
@@ -48,6 +74,6 @@ fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier =
 @Composable
 fun DefaultPreview() {
     HappyBirthday_ComposeTheme {
-        BirthdayGreetingWithText("Happy birthday, Yoba", "yeboba")
+        BirthdayGreetingWithImage("Happy birthday, Yoba", "yeboba")
     }
 }
